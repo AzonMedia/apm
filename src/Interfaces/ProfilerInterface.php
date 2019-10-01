9f88ab9eb8cfdf1a2958dc828c49cf46832c8068
Vesko Kenashkov <vesko@azonmedia.com>
2019-10-01 11:48:04 +0300

    fixes

---
+++ src/Interfaces/ProfilerInterface.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\Apm\Interfaces;
+interface ProfilerInterface
+{
+    public const PROFILE_STRUCTURE = [
+        'worker_id'                 => 0,
+        'coroutine_id'              => 0,
+        'execution_start_microtime' => 0,
+        'execution_end_microtime'   => 0,
+        'cnt_used_connections'      => 0,
+        'time_used_connections'     => 0,//for all connections - for how long the connection was held
+        'time_waiting_for_connection'   => 0,//waiting time to obtain connection from the Pool
+        'cnt_total_coroutines'      => 0,
+        'cnt_subcoroutines'         => 0,
+        'cnt_dql_statements'        => 0,
+    
+    ];
+
+}
--- src/Profiler.php
+++ src/Profiler.php
@@ -6,30 +6,23 @@ namespace Azonmedia\Apm;
 class Profiler implements ProfilerInterface
 {
 
-    protected $profile_data = [
-	'execution_start_microtime'	=> 0,
-	'execution_end_microtime'	=> 0,
-	'cnt_used_connections'		=> 0,
-	'time_used_connections'		=> 0,//for all connections
-	'cnt_dql_statements'	=> 0,
-	
-    ];
+    protected $profile_data = ProfilerInterface::PROFILE_STRUCTURE;
 
     protected $backends = [];
 
     public function __construct(BackendInterface $Backend)
     {
-	$this->add_backend($Backend);
+        $this->add_backend($Backend);
     }
     
     public function __destruct()
     {
-	$this->store();
+        $this->store();
     }
     
     public function add_backend(BackendInterface $Backend) : void
     {
-	$this->backends[] = $Backend;
+        $this->backends[] = $Backend;
     }
     
     public function increment_value(string $key, float $amount) : void
@@ -37,13 +30,16 @@ class Profiler implements ProfilerInterface
     
     }
 
-    
+    public function reset_data()
+    {
+        $this->profile_data = ProfilerInterface::PROFILE_STRUCTURE;
+    }
     
     protected function store()
     {
-	foreach ($this->backends as $Backend) {
-	    $Backend->store_data($this->profile_data);
-	}
+        foreach ($this->backends as $Backend) {
+            $Backend->store_data($this->profile_data);
+        }
     }
     
     
