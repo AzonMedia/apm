5dc714515b3cec5abd0b0cf449aa9e86861b0b6f
Vesko Kenashkov <vesko@azonmedia.com>
2020-08-31 12:48:57 +0300

    - small fixes - coding style

--- src/Profiler.php
+++ src/Profiler.php
@@ -23,6 +23,7 @@ class Profiler implements ProfilerInterface
     //public function __construct(BackendInterface $Backend, int $worker_id = -1)
     public function __construct(BackendInterface $Backend, array $profile_data_structure, int $worker_id = -1)
     {
+        //debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
         $this->add_backend($Backend);
         $this->profile_data = $profile_data_structure;
 
@@ -34,12 +35,13 @@ class Profiler implements ProfilerInterface
 //        } else {
 //            $worker_id = -1;
 //        }
-        $this->profile_data['worker_pid'] = getmypid();//currently worker_id is no longer available
+
+        $worker_pid = getmypid();//currently worker_id is no longer available in Swoole unless there is access to the Server object so we also provide the pid
+        $this->profile_data['worker_pid'] = $worker_pid;
+        $this->profile_data['worker_id'] = $worker_id;
         if (self::get_cid() > 0) {
-            $this->profile_data['worker_id'] = $worker_id;
             $this->profile_data['coroutine_id'] = \Swoole\Coroutine::getCid();
         } else {
-            $this->profile_data['worker_id'] = -1;
             $this->profile_data['coroutine_id'] = -1;
         }
         $this->profile_data['execution_start_microtime'] = microtime(TRUE) * 1_000_000;
@@ -57,17 +59,31 @@ class Profiler implements ProfilerInterface
         $this->store_data_end_time();
     }
 
-    public function __toString() : string
+    public function __toString(): string
     {
-        return print_r($this->profile_data, TRUE);
+        return print_r($this->get_data(), TRUE);
+    }
+
+    /**
+     * Allows new profile data structure entries to be added during runtime
+     * @param string $key
+     */
+    public function add_key(string $key): void
+    {
+        if (!$key) {
+            throw new \InvalidArgumentException(sprintf('There is no key provided.'));
+        }
+        if (!array_key_exists($key, $this->profile_data)) {
+            $this->profile_data[$key] = 0;
+        }
     }
     
-    public function add_backend(BackendInterface $Backend) : void
+    public function add_backend(BackendInterface $Backend): void
     {
         $this->backends[] = $Backend;
     }
     
-    public function increment_value(string $key, float $amount) : void
+    public function increment_value(string $key, float $amount): void
     {
         if (!array_key_exists($key, $this->profile_data)) {
             throw new \InvalidArgumentException(sprintf('There is no key named "%s" in the $profile_data.', $key));
@@ -75,12 +91,12 @@ class Profiler implements ProfilerInterface
         $this->profile_data[$key] += $amount;
     }
 
-    public function reset_data() : void
+    public function reset_data(): void
     {
         $this->profile_data = ProfilerInterface::PROFILE_STRUCTURE;
     }
     
-    public function store_data() : void
+    public function store_data(): void
     {
         if ($this->is_data_stored()) {
             return;
@@ -91,19 +107,19 @@ class Profiler implements ProfilerInterface
         $this->data_stored_flag = TRUE;
     }
 
-    public function store_data_end_time() : void
+    public function store_data_end_time(): void
     {
         foreach ($this->backends as $Backend) {
             $Backend->store_data_end_time($this->profile_data);
         }
     }
 
-    public function is_data_stored() : bool
+    public function is_data_stored(): bool
     {
         return $this->data_stored_flag;
     }
 
-    public function get_data() : array
+    public function get_data(): array
     {
         return $this->profile_data;
     }
